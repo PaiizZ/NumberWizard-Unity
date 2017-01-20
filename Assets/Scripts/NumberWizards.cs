b934ffbd5e072def27ac89ce91d6540823469f69
paiizz <wanchanapon.t@ku.th>
2017-01-20 11:19:30 +0700

    Initail Project

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("PaiizZ");
+		print ("Fah");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
